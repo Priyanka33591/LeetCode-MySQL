a2921fbf45c87df669821819ed7236e95302349a
Priyanka33591 <priyanka33591@gmail.com>
2025-10-14 22:56:59 +0530

    add some new solution

---
+++ 1965. Employees With Missing Information/solution.sql
@@ -0,0 +1,13 @@
+# Write your MySQL query statement below
+SELECT Employees.employee_id
+FROM Employees
+LEFT JOIN Salaries
+ON Employees.employee_id =  Salaries.employee_id
+WHERE Salaries.salary IS NULL
+UNION
+SELECT Salaries.employee_id
+FROM  Salaries
+LEFT JOIN Employees
+ON Salaries.employee_id = Employees.employee_id
+WHERE Employees.name IS NULL
+ORDER BY employee_id;
